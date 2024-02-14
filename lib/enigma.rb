8c7dcebfaff08d571ddf611aa2ec248b24670a0d
Antoine Aube <aaube3@gmail.com>
2024-02-14 14:53:50 -0700

    initial Enigma class with generate offset method

---
+++ lib/enigma.rb
@@ -0,0 +1,42 @@
+require 'date'
+
+class Enigma
+  def encrypt()
+    
+  end
+
+  def alphabet
+    ("a".."z").to_a << ""
+  end
+
+  def generate_keys
+
+  end
+
+  def generate_offset(date)
+    if date !=nil
+      date = Date.today
+      formatted = date.strftime('%d%m%y')
+      digits = formatted.to_i
+      squared = digits ** 2
+      last_four = squared.to_s[-4..]
+    end
+    date
+  end
+end
+
+# date = Date.today
+
+# formatted = date.strftime('%d%m%y')
+
+# puts formatted.to_i
+
+# alphabet = ("a".."z").to_a << ""
+
+# pp alphabet
+
+# random_number = format('%05d', rand(0..99999))
+
+# pp random_number
+
+pp Enigma.new.generate_offset
