c05b535e5c4348e520f131f62f590c5da933e694
Antoine Aube <aaube3@gmail.com>
2024-02-15 15:39:00 -0700

    instance methods generate_numeric_date, generate_offset, generate_keys, generate aplhabet

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,18 +10,22 @@ class Enigma
   end
 
   def generate_keys
-
+    random_four_digits = rand(1000..9999)
+    "0#{random_four_digits}"
   end
 
   def generate_offset(date)
-    if date !=nil
-      date = Date.today
-      formatted = date.strftime('%d%m%y')
-      digits = formatted.to_i
-      squared = digits ** 2
-      last_four = squared.to_s[-4..]
+    if date == nil
+      squared = generate_numeric_date ** 2
+      return last_four = squared.to_s[-4..]
     end
     date
+  end
+
+  def generate_numeric_date
+    date = Date.today
+    formatted = date.strftime('%d%m%y')
+    digits = formatted.to_i
   end
 end
 
@@ -33,7 +37,7 @@ end
 
 # alphabet = ("a".."z").to_a << ""
 
-# pp alphabet
+pp Enigma.new.generate_offset(nil)
 
 # random_number = format('%05d', rand(0..99999))
 
