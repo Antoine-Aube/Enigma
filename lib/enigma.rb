376d98aaec60608d9d3760c4ef8ff29a5fa31f1d
Antoine Aube <aaube3@gmail.com>
2024-02-16 14:08:02 -0700

    refactor: all methods to be decoupled and useable in main encryption function

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,42 +2,57 @@ require 'date'
 
 class Enigma
   def encrypt(string, key = nil, date = nil )
+    encrypted_string = []
+    digits = random_digits
+    if date == nil
+      date = date_generator
+    end 
+
+    if key == nil
+      key = generate_keys(digits)
+    end
+
+    offset = generate_offest(date)
     
+    shifts = generate_shifts(key, offset)
+      
+    output = {
+      encryption: encrypted_string,
+      key: key,
+      date: date
+    }
   end
 
   def alphabet
     ("a".."z").to_a << ""
   end
 
-  def generate_keys
+def random_digits
     random_four_digits = rand(1000..9999)
     numeric_string = "0#{random_four_digits}"
+end 
+
+  def generate_keys(random_digits)
     {
-      "A" => (numeric_string[0, 2]).to_i,
-      "B" => (numeric_string[1, 2]).to_i,
-      "C" => (numeric_string[2, 2]).to_i,
-      "D" => (numeric_string[3, 2]).to_i
+      "A" => (random_digits[0, 2]).to_i,
+      "B" => (random_digits[1, 2]).to_i,
+      "C" => (random_digits[2, 2]).to_i,
+      "D" => (random_digits[3, 2]).to_i
     }
   end
 
-  def generate_offset(date)
-    if date == nil
-      squared = generate_numeric_date ** 2
-      return last_four = squared.to_s[-4..]
-    end
-    date
+  def generate_offset(date_generator)
+    squared = date_generator ** 2
+    last_four = squared.to_s[-4..]
   end
 
   def generate_numeric_date
     date = Date.today
     formatted = date.strftime('%d%m%y')
-    # require 'pry';binding.pry
     digits = formatted.to_i
   end
 
-  def generate_shifts(date)
-    offset = generate_offset(date)
-    keys = generate_keys
+  def generate_shifts(keys, offset)
     {
       "A" => keys["A"] + offset[0].to_i,
       "B" => keys["B"] + offset[1].to_i,
@@ -63,7 +78,7 @@ end
 # pp offset
 # pp shifts
 # require 'pry';binding.pry
-pp Enigma.new.generate_offset(nil)
+# pp Enigma.new.generate_offset(nil)
 
 
 
