b75b557f91a4ad8046bb77f1cfad95565e476627
Antoine Aube <aaube3@gmail.com>
2024-02-16 15:56:13 -0700

    test: encryption method test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Enigma do
   describe "instance methods" do 
     it "has an aplhabet method that generates the alaphabet" do 
       expect(enigma.alphabet).to be_a(Array)
-      expect(enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ""])
+      expect(enigma.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
     end
 
     it "#generate_offset" do
@@ -43,19 +43,32 @@ RSpec.describe Enigma do
       digits = enigma.random_digits
       key = enigma.generate_keys(digits)
       offset = enigma.generate_offset(generated_date)
-
-
+      
+      
       shifts = enigma.generate_shifts(key, offset)
-
+      
       expect(shifts["A"]).to eq(1)
       expect(shifts["B"]).to eq(18)
       expect(shifts["C"]).to eq(25)
       expect(shifts["D"]).to eq(39)
     end
   end
-
-  xdescribe "encryption" do
+  
+  describe "encryption" do
     it "main encryption method" do
+      allow_any_instance_of(Date).to receive(:strftime).with('%d%m%y').and_return("120225")
+      allow_any_instance_of(Object).to receive(:rand).with(1000..9999).and_return(1234)
+
+      string = "hello world"
+      key = "02715"
+      date = "040895"
+
+      expected_output  = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+      expect(enigma.encrypt(string, key, date)). to eq(expected_output)
 
     end
   end
